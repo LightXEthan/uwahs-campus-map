9ad97cbfca2a7599d885a2f4903e4ac2cfc5e857
jkkim <j.k.kim98@outlook.com>
2019-08-25 19:16:57 +0800

    basic react app

--- README.old.md
+++
@@ -1,4 +0,0 @@
-# CITS3200Project_23
-
-## Meetings
-Find the meeting agendas and minutes at the [wiki](https://github.com/LightXEthan/CITS3200Project_23/wiki).
---
+++ src/components/Firebase/firebase.js
@@ -0,0 +1,24 @@
+import app from "firebase/app";
+import "firebase/auth";
+
+require("dotenv").config();
+
+const config = {
+  apiKey: process.env.REACT_APP_APIKEY,
+  authDomain: process.env.REACT_APP_AUTHDOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASEURL,
+  projectId: process.env.REACT_APP_PROJECTID,
+  storageBucket: process.env.REACT_APP_STORAGEBUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
+  appId: process.env.REACT_APP_APPID
+};
+
+class Firebase {
+  constructor() {
+    app.initializeApp(config);
+
+    this.auth = app.auth();
+  }
+}
+
+export default Firebase;
