7159b1766a891b0bc9f30931112562cd1a8edae0
Lee Huey Charn <hueycharn.lee@gmail.com>
2019-09-09 21:22:42 +0800

    add signout function

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -1,9 +1,10 @@
 import React, { Component, Fragment } from "react";
 import { NavLink as RRNavLink } from "react-router-dom";
 
+import SignOutButton from '../SignOut';
 import * as ROUTES from "../../constants/routes";
 
-import { Navbar, NavbarBrand, Nav, NavItem, NavLink } from "reactstrap";
+import { Navbar, NavbarBrand, Nav, NavItem, NavLink, Button } from "reactstrap";
 
 class Navigation extends Component {
   constructor(props) {
@@ -24,6 +25,11 @@ class Navigation extends Component {
               <NavLink tag={RRNavLink} exact to={ROUTES.ABOUT}>
                 About
               </NavLink>
+            </NavItem>
+          </Nav>
+          <Nav className="ml-auto" navbar>
+            <NavItem>
+              <SignOutButton />
             </NavItem>
           </Nav>
         </Navbar>
