64e59a8cebde124c2cc27ac9e84a6c6ce2f0af37
Lee Huey Charn <hueycharn.lee@gmail.com>
2019-09-09 22:06:46 +0800

    add session handling

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from "react";
+import React, { Fragment, Component } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import MapPage from "../MapPage";
@@ -7,22 +7,43 @@ import Navigation from "../Navigation";
 import SignInPage from "../SignIn";
 import AdminPage from "../Admin";
 import PasswordForgetPage from "../PasswordForget";
-
-import { withFirebase } from "../Firebase";
 import * as ROUTES from "../../constants/routes";
 
-const App = () => (
-  <Router>
-    <Fragment>
-      <Navigation />
-      <Route exact path={ROUTES.MAP} component={MapPage} />
-      <Route path={ROUTES.ABOUT} component={AboutPage} />
-      <Route path={ROUTES.SIGN_IN} component={SignInPage} />
-      <Route path={ROUTES.ADMIN} component={AdminPage} />
-      <Route path={ROUTES.PASSWORD_FORGET} component={PasswordForgetPage} />
-    </Fragment>
-  </Router>
-);
+import { withFirebase } from "../Firebase";
 
-// export default withFirebase(App);
-export default App;
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      authUser: null
+    };
+  }
+
+  componentDidMount() {
+    this.listener = this.props.firebase.auth.onAuthStateChanged(authUser => {
+      authUser ? this.setState({ authUser }) : this.setState({ authUser: null });
+    });
+  }
+
+  // remove listener to avoid memory leaks
+  componentWillUnmount() {
+    this.listener();
+  }
+
+  render() {
+    return (
+      <Router>
+        <Fragment>
+          <Navigation authUser={this.state.authUser} />
+          <Route exact path={ROUTES.MAP} component={MapPage} />
+          <Route path={ROUTES.ABOUT} component={AboutPage} />
+          <Route path={ROUTES.SIGN_IN} component={SignInPage} />
+          <Route path={ROUTES.ADMIN} component={AdminPage} />
+          <Route path={ROUTES.PASSWORD_FORGET} component={PasswordForgetPage} />
+        </Fragment>
+      </Router>
+    );
+  }
+}
+
+export default withFirebase(App);
--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -4,38 +4,49 @@ import { NavLink as RRNavLink } from "react-router-dom";
 import SignOutButton from '../SignOut';
 import * as ROUTES from "../../constants/routes";
 
-import { Navbar, NavbarBrand, Nav, NavItem, NavLink, Button } from "reactstrap";
+import { Navbar, NavbarBrand, Nav, NavItem, NavLink } from "reactstrap";
 
-class Navigation extends Component {
-  constructor(props) {
-    super(props);
+const Navigation = ({ authUser }) => (
+  <div>{authUser ? <NavigationAuth /> : <NavigationNonAuth />}</div>
+);
 
-    this.state = {};
-  }
+const NavigationAuth = () => (
+  <Fragment>
+      <Navbar color="light" expand="md">
+        <NavbarBrand tag={RRNavLink} exact to={ROUTES.MAP}>
+          Map
+        </NavbarBrand>
+        <Nav>
+          <NavItem>
+            <NavLink tag={RRNavLink} exact to={ROUTES.ABOUT}>
+              About
+            </NavLink>
+          </NavItem>
+        </Nav>
+        <Nav className="ml-auto" navbar>
+          <NavItem>
+            <SignOutButton />
+          </NavItem>
+        </Nav>
+      </Navbar>
+    </Fragment>
+);
 
-  render() {
-    return (
-      <Fragment>
-        <Navbar color="light" expand="md">
-          <NavbarBrand tag={RRNavLink} exact to={ROUTES.MAP}>
-            Map
-          </NavbarBrand>
-          <Nav>
-            <NavItem>
-              <NavLink tag={RRNavLink} exact to={ROUTES.ABOUT}>
-                About
-              </NavLink>
-            </NavItem>
-          </Nav>
-          <Nav className="ml-auto" navbar>
-            <NavItem>
-              <SignOutButton />
-            </NavItem>
-          </Nav>
-        </Navbar>
-      </Fragment>
-    );
-  }
-}
+const NavigationNonAuth = () => (
+  <Fragment>
+    <Navbar color="light" expand="md">
+      <NavbarBrand tag={RRNavLink} exact to={ROUTES.MAP}>
+        Map
+      </NavbarBrand>
+      <Nav>
+        <NavItem>
+          <NavLink tag={RRNavLink} exact to={ROUTES.ABOUT}>
+            About
+          </NavLink>
+        </NavItem>
+      </Nav>
+    </Navbar>
+  </Fragment>
+);
 
 export default Navigation;
