77f82df3478624b17ba5092f29c0d4c170b731e9
LightXEthan <22248878@student.uwa.edu.au>
2019-09-15 14:48:09 +0800

    Adds file metadata collection when adding poi

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -29,7 +29,7 @@ class Firebase {
   // Points of interest firestore database
   poi = () => this.db.collection("poi").doc();
   pois = () => this.db.collection("poi");
-  poif = doc => this.db.collection("poi").doc(doc).collection("files");
+  poif = poiid => this.db.collection("poi").doc(poiid).collection("files");
   poiUpdate = poiid => this.db.collection("poi").doc(poiid);
 
   // *** Auth API ***
