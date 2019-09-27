0441a330da3139d8e2413ed106862895cfed2c2b
jkkim <j.k.kim98@outlook.com>
2019-09-28 01:18:30 +0800

    remove navbar for unauthenticated

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,21 +1,23 @@
-import React, { Fragment} from "react";
+import React, { Fragment } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
+import { withAuthentication } from "../Session";
+import AuthUserContext from "../Session/context";
+
 import MapPage from "../MapPage";
-import AboutPage from "../About";
 import Navigation from "../Navigation";
 import SignInPage from "../SignIn";
 import AdminPage from "../Admin";
 import PasswordForgetPage from "../PasswordForget";
 import * as ROUTES from "../../constants/routes";
-import { withAuthentication } from "../Session";
 
 const App = () => (
   <Router>
     <Fragment>
-      <Navigation />
+      <AuthUserContext.Consumer>
+        {authUser => (authUser ? <Navigation /> : null)}
+      </AuthUserContext.Consumer>
       <Route exact path={ROUTES.MAP} component={MapPage} />
-      <Route path={ROUTES.ABOUT} component={AboutPage} />
       <Route path={ROUTES.SIGN_IN} component={SignInPage} />
       <Route path={ROUTES.ADMIN} component={AdminPage} />
       <Route path={ROUTES.PASSWORD_FORGET} component={PasswordForgetPage} />
--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -11,39 +11,34 @@ import { Navbar, NavbarBrand, Nav, NavItem, NavLink } from "reactstrap";
 const Navigation = () => (
   <div>
     <AuthUserContext.Consumer>
-      {authUser => authUser ? <NavigationAuth /> : <NavigationNonAuth />}
+      {authUser => (authUser ? <NavigationAuth /> : <NavigationNonAuth />)}
     </AuthUserContext.Consumer>
   </div>
 );
 
 const NavigationAuth = () => (
   <Fragment>
-      <Navbar color="light" expand="md">
-        <NavbarBrand tag={RRNavLink} exact to={ROUTES.MAP}>
-          Map
-        </NavbarBrand>
-        <Nav>
-          <NavItem>
-            <NavLink tag={RRNavLink} exact to={ROUTES.ABOUT}>
-              About
-            </NavLink>
-          </NavItem>
-          <NavItem>
-            <NavLink tag={RRNavLink} exact to={ROUTES.ADMIN}>
-              Admin
-            </NavLink>
-          </NavItem>
-        </Nav>
-        <Nav className="ml-auto" navbar>
-          <NavItem>
-            <PasswordChange />
-          </NavItem>
-          <NavItem>
-            <SignOutButton />
-          </NavItem>
-        </Nav>
-      </Navbar>
-    </Fragment>
+    <Navbar color="light" expand="md">
+      <NavbarBrand tag={RRNavLink} exact to={ROUTES.MAP}>
+        Map
+      </NavbarBrand>
+      <Nav>
+        <NavItem>
+          <NavLink tag={RRNavLink} exact to={ROUTES.ADMIN}>
+            Admin
+          </NavLink>
+        </NavItem>
+      </Nav>
+      <Nav className="ml-auto" navbar>
+        <NavItem>
+          <PasswordChange />
+        </NavItem>
+        <NavItem>
+          <SignOutButton />
+        </NavItem>
+      </Nav>
+    </Navbar>
+  </Fragment>
 );
 
 const NavigationNonAuth = () => (
