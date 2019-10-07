df45704dea567a0e228467ecd22e9d3d48bff453
jkkim <j.k.kim98@outlook.com>
2019-10-07 16:29:49 +0800

    add search functionality

--- src/components/MapPOIList/index.js
+++ src/components/MapPOIList/index.js
@@ -1,17 +1,63 @@
-import React, { Component, Fragment } from "react";
+import React, { Component } from "react";
 
 import { ListGroup, ListGroupItem, Button } from "reactstrap";
 
+import Fuse from "fuse.js";
+
+const options = {
+  shouldSort: true,
+  threshold: 0.6,
+  location: 0,
+  distance: 100,
+  maxPatternLength: 32,
+  minMatchCharLength: 1,
+  keys: [{ name: "name", weight: 1.0 }, { name: "description", weight: 0.3 }]
+};
+
 class MapPOIList extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      searchRes: []
+    };
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.POIList !== this.state.searchRes) {
+      this.setState({
+        searchRes: [...nextProps.POIList]
+      });
+    }
+    this.fuse = new Fuse(this.props.POIList, options);
+  }
+
+  handleInputChange = e => {
+    let stuff =
+      e.target.value !== ""
+        ? this.fuse.search(e.target.value)
+        : this.props.POIList;
+    this.setState({
+      searchRes: [...stuff]
+    });
+  };
+
   render() {
     const { POIList, onListItemClick } = this.props;
 
     return (
-      <Fragment>
+      <>
+        <form>
+          <input
+            placeholder="Search for a location..."
+            onChange={this.handleInputChange}
+          ></input>
+        </form>
+
         <ListGroup
           style={{ overflow: "auto", height: `${window.innerHeight - 56}px` }}
         >
-          {POIList.map(poiitem => (
+          {this.state.searchRes.map(poiitem => (
             <ListGroupItem key={poiitem._id}>
               <Button
                 key={poiitem._id}
@@ -25,7 +71,7 @@ class MapPOIList extends Component {
             </ListGroupItem>
           ))}
         </ListGroup>
-      </Fragment>
+      </>
     );
   }
 }
