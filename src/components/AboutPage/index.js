5ffe1b66849da33bf3c231558bb5210090abd45d
Lee Huey Charn <hueycharn.lee@gmail.com>
2019-10-17 16:52:13 +0800

    fix spacing

--- src/components/AboutPage/index.js
+++ src/components/AboutPage/index.js
@@ -18,10 +18,10 @@ const AboutPage = () => (
           </div>
           <h2>About</h2>
           <p>
-            This app was built for the UWA Historical Society as a project for
+            This app was built for the UWA Historical Society as a project for&nbsp; 
             <a href="http://teaching.csse.uwa.edu.au/units/CITS3200/">
-              CITS3200 Professional Computing
-            </a>
+              CITS3200 Professional Computing&nbsp; 
+            </a> 
             2019.
           </p>
           <h2>Group 23</h2>
